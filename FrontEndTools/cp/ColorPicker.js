1426e3c5aaff03d51fa6409835a18a477ca41894
nss-2102 <159945955+nss-2102@users.noreply.github.com>
2024-05-09 12:37:14 +0530

    Delete FrontEndTools directory

--- FrontEndTools/cp/ColorPicker.js
+++
@@ -1,9 +0,0 @@
-function selectColor() {
-    var selectedColor = document.getElementById("colorPicker").value;
-    document.querySelector(".column").style.backgroundColor = selectedColor;
-
-    document.getElementById("hexCode").value = selectedColor;
-  }
-  document
-    .getElementById("colorPicker")
-    .addEventListener("input", selectColor);
